4cb43dd3e7e0537b191c2e8b848baec117c8d1cc
Kevin Duran <kevinduraan173@gmail.com>
2023-01-29 19:02:52 -0600

    Fix static assets not found 2

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,12 +1,10 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    // Same behavior as the "src" example above:
-    src: {url: '/_dist_'},
-    // Mount "public" to the root URL path ("/*") and serve files with zero transformations:
-    public: {url: '/', static: true, resolve: false},
+    public: '/',
+    src: '/_dist_',
   },
   buildOptions: {
-    baseUrl: 'https://kevindur4n.github.io/AvocadoDOM/',
+    baseUrl: 'https://kevindur4n.github.io/AvocadoDOM',
   },
 }
